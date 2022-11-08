8740bc43e84d80e15b1722c6ec4ab6b54b554241
student <myhajliop@gmail.com>
2022-11-08 19:27:47 +0200

    fhgfd

--- main.py
+++ main.py
@@ -5,3 +5,36 @@ class Wow:
             print("hello")
             soffia = Wow()
             soffia._hello()
+            soffia._wow()
+
+class Hello:
+                def __init__(self):
+                    print("Hello!!!")
+class Hello_World(Hello):
+                        def __init__(self):
+                            super().__init__()
+                            print("   World")
+hello_world = Hello_World()
+
+class Computer:
+    def calculete(selfself):
+        print(".....рахую.....")
+class Display:
+    def display(self):
+        print("шось там показує")
+class SmartPhone(Computer, Display):
+    ...
+samsung = SmartPhone()
+samsung.calculete()
+samsung.display()
+
+import requests
+help(requests)
+def first_function():
+    ...
+class Site:
+    ...
+ITSTEP = Site
+print(" FOR Misha " , requests.__cake__)
+
+print(type(" kuku "))
